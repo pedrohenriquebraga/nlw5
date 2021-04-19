157b6cd2c4e0afe4b2444f2bf0ae8658073d5f30
pedrohenriquebraga <gamesantos7@gmail.com>
2021-04-19 09:43:33 -0300

    Class 1, start here

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
+import wateringImg from "../assets/watering.png";
+import { Button } from "../components/Button";
+import colors from "../styles/colors";
+
+export function Welcome() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {"\n"}
+        suas plantas {"\n"}
+        de forma fácil!
+      </Text>
+
+      <Image source={wateringImg} style={styles.image} />
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+      <Button title=">" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+
+  subtitle: {
+    textAlign: "center",
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading,
+  },
+  image: {
+    width: 292,
+    height: 284,
+  },
+});
